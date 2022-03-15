bacc003644260bf6e72c211ed5b2458fdb016ef9
juimodi27 <juimodi99@gmail.com>
2022-03-15 17:51:47 +0530

    commands

---
+++ typescript/Day 15/ex1types.ts
@@ -0,0 +1,7 @@
+let user : string = "Alexandar";
+let age : number = 35;
+let isEmployed : boolean = true;
+let contacts : any = {phone: 99999, email: "alex@gmail.com"};
+
+document.write(`<p>Username:${user}, Age:${age}, isEmployed: ${isEmployed}</p>`);
+document.write(`<p>Phone: ${contacts.phone}, Email: ${contacts.email}</p>`);
