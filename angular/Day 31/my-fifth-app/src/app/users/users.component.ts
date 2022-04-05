4e93c79bf4c6ccc3c31c72772300dca3121152b0
juimodi27 <juimodi99@gmail.com>
2022-04-05 17:48:05 +0530

    day 31

---
+++ angular/Day 31/my-fifth-app/src/app/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  users : any = [
+    {name : "Alexander", gender : "Male", address : {state : "KA", city : "BLR"}},
+    {name : "Sony", gender : "Female", address : {state : "MH", city : "MBI"}},
+    {name : "Raj", gender : "Male", address : {state : "KA", city : "MYS"}}
+  ]
+}
